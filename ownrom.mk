35e0433bdfba507f178df82b44aad5c9f778cf70
vidhanth <vidhanthbafna@gmail.com>
2016-05-14 15:09:54 +0000

    Ownify Necessary changes to build OwnROM

--- ownrom.mk
+++ ownrom.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common OwnROM stuff.
+$(call inherit-product, vendor/ownrom/config/common_full_phone.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := cm_titan
+PRODUCT_NAME := ownrom_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
